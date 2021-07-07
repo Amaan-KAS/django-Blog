f2087f3d6a30775ecf5d6c682f1f7518ed258176
Amaan-KAS <amaanahmedkashif@gmail.com>
2021-07-07 16:31:09 +0530

    Database and Migrations

--- blog/views.py
+++ blog/views.py
@@ -1,33 +1,12 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 # from django.http import HttpResponse
 
-posts = [
-
-    {
-        'author': 'CoreyMS',
-        'title' : 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted':'June 27, 2021'
-    },
-    {
-        'author': 'JoreyMS',
-        'title' : 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted':'July 28, 2021'
-    },
-        {
-        'author': 'MS',
-        'title' : 'Blog Post 3',
-        'content': 'Third post content',
-        'date_posted':'August 29, 2021'
-    }
-]
-
 def home(request):
     context = {
-        'posts': posts
+        # 'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
